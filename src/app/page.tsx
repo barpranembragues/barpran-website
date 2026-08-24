f4a5aab55df60dfe038f5ccca9c62da5c8381592
barpranembragues <barpranembragues@gmail.com>
2026-08-24 10:00:36 -0300

    Integra pilotos y reparaciones en home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/sections/Hero";
+import PilotosBarpran from "@/components/sections/PilotosBarpran";
 import Manifesto from "@/components/sections/Manifesto";
 import Engineering from "@/components/sections/Engineering";
 import Motorsport from "@/components/sections/Motorsport";
 import Products from "@/components/sections/Products";
 import Manufacturing from "@/components/sections/Manufacturing";
+import ReparacionesEstandar from "@/components/sections/ReparacionesEstandar";
 import History from "@/components/sections/History";
 import Shop from "@/components/sections/Shop";
 import Contact from "@/components/sections/Contact";
@@ -15,16 +17,17 @@ export default function Home() {
     <>
       <ScrollProgress />
       <Hero />
+      <PilotosBarpran />
       <Manifesto />
       <Engineering />
       <Motorsport />
       <Products />
+      <ReparacionesEstandar />
       <Manufacturing />
       <History />
       <Shop />
       <AplicacionesEspeciales />
       <Contact />
-      
     </>
   );
 }
